963548002ac948de13fd5bb6a072f59210078973
zhaobinglong <binglong1992@gmail.com>
2020-01-27 20:31:57 -0800

    build

--- config/index.js
+++ config/index.js
@@ -6,9 +6,9 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'dist/static',
+    assetsSubDirectory: 'static',
     // assetsPublicPath: '/',
-    assetsPublicPath: '/unibbs-wechat/unibbs/',
+    assetsPublicPath: '/unibbs-wechat/unibbs/dist/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
